a97b25a9f410c8e44a5b6d4bd71e59d548e0160c
Krishnendu M R <krishnendumr18@gmail.com>
2024-12-28 00:52:23 +0530

    initial app added

---
+++ Backend/eslint.config.js
@@ -0,0 +1,50 @@
+const globals = require('globals')
+const stylisticJs = require('@stylistic/eslint-plugin-js')
+const js = require('@eslint/js')
+
+module.exports = [
+  js.configs.recommended,
+  {
+    files: ['**/*.js'],
+    languageOptions: {
+      sourceType: 'commonjs',
+      globals: {
+        ...globals.node,
+      },
+      ecmaVersion: 'latest',
+    },
+    plugins: {
+      '@stylistic/js': stylisticJs,
+    },
+    rules: {
+      '@stylistic/js/indent': [
+        'error',
+        2
+      ],
+      '@stylistic/js/linebreak-style': [
+        'error',
+        'unix'
+      ],
+      '@stylistic/js/quotes': [
+        'error',
+        'single'
+      ],
+      '@stylistic/js/semi': [
+        'error',
+        'never'
+      ],
+      'eqeqeq': 'error',
+      'no-trailing-spaces': 'error',
+      'object-curly-spacing': [
+        'error', 'always'
+      ],
+      'arrow-spacing': [
+        'error', { 'before': true, 'after': true },
+      ],
+      'no-console': 'off',
+    },
+  },
+  {
+    ignores: ['dist/**', 'build/**'],
+  },
+]
--- Krishnendu.md
+++ Krishnendu.md
@@ -1,2 +1,11 @@
 # Workflow - Krishnendu M R
-1. Express App Creation - started on ```27 DEC 2024``` - ended on ```not yet completed``` - <span style="color:green;">not verified.</span>
+1. Express App Creation - started on ```27 DEC 2024``` - ended on ```27 DEC 2024``` - <span style="color:green;">not verified.</span>
+2. Models creation - started on ```28 DEC 2025``` - ended on ``` ongoing ``` - not verified
+3. Routers creation - started on ```28 DEC 2025``` - ended on ```ongoing``` - not verified
+4. Testing app - 
+5. Testing database - 
+6. Testing routers - 
+7. Production database testing - 
+8. Production app testing - 
+9. Authentication updation -
+10. Payment integration - 
