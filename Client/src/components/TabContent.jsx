90e42491447677db9e7e08f11b3fd2cb32aeb0c6
Vimalesh C T <pranovvimal30@gmail.com>
2025-03-02 22:21:12 +0530

    added accomodation error

--- Client/src/components/TabContent.jsx
+++ Client/src/components/TabContent.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import { FaDownload } from "react-icons/fa";
 import { FaPhoneAlt } from "react-icons/fa";
-
 function TabContent({ activeTab, petData }) {
   return (
     <div className="tab-content">
+      {/* <h2>{petData[activeTab].animal}</h2> */}
       <p>{petData[activeTab].fact}</p>
 
       {/* Display additional details for FAQ tab */}
@@ -12,15 +12,16 @@ function TabContent({ activeTab, petData }) {
         <div className="faq-details">
           <h3>Contact Details:</h3>
           <h4 className="drop">
-            Drop mail at{" "}
-            <a href="mailto:saasceg25@gmail.com" className="email-link">
+            Drop mail at {}
+            <a href="mailto:support@example.com" className="email-link">
               saasceg25@gmail.com
             </a>
           </h4>
           <ul>
             {petData[activeTab].details.map((detail, index) => (
               <li key={index}>
-                <FaPhoneAlt /> {detail.name} - {detail.number}
+                <FaPhoneAlt />
+                {detail.name} - {detail.number}
               </li>
             ))}
           </ul>
@@ -32,11 +33,12 @@ function TabContent({ activeTab, petData }) {
         petData[activeTab].downloadLink && (
           <div className="download-section">
             <a
-              href="../assets/rule.pdf"
-              download="rule.pdf"
+              href={petData[activeTab].downloadLink}
+              download
               className="download-button"
             >
-              <FaDownload /> Download Rules
+              <FaDownload />
+              Download Rules
             </a>
           </div>
         )}
