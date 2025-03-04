7355a881162b81af28f9dbf922ef8f9bfd2d0607
Krishnendu M R <130251084+KrishnenduMR@users.noreply.github.com>
2025-03-04 14:40:26 +0000

    increased limit

--- Server/app.js
+++ Server/app.js
@@ -29,7 +29,7 @@ mongoose
 app.use(cors());
 
 app.use(express.static("dist"));
-app.use(express.json());
+app.use(express.json({ limit: "50mb" }));
 app.use(middleware.requestLogger);
 
 // Auth Routes
